d2dd4be3dc59a582c9ca5690985ff27b4f60ebf6
Mitiii <srp5628comust@gmail.com>
2025-05-27 15:02:22 +0900

    fix: viteのserverを修正

--- src/vite.config.js
+++ src/vite.config.js
@@ -10,13 +10,9 @@ export default defineConfig({
         },
     },
     server: {
-        host: true,
-        hmr: {
-            host: 'localhost',
-        },
-        watch: {
-            usePolling: true,
-        },
+        host: '127.0.0.1',
+        port: 5173,
+        origin: 'http://127.0.0.1:5173'
     },
     plugins: [
         laravel({
